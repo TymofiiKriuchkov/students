79ccf61d05a8777bed7ee692b3ebc9fb80c3cc8a
Tymofii <TymofiiKriuchkov@users.noreply.github.com>
2018-05-31 15:13:40 +0300

    Update Validator_2.kt

--- challenge_1/Validator_2.kt
+++ challenge_1/Validator_2.kt
@@ -1,10 +1,4 @@
 
 fun validateInputIsValidUrl(input : String) : Boolean {
-    val maxLength = 100
-    if(input.length > maxLength) {
-        return false
-    }
-
-    val URL_PATTERN = Pattern.compile("^(http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?\$")
-    //return regex check is true
+    val URL_PATTERN = Pattern.compile("^(http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?{0,100}$")
 }
